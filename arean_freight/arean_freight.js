b4a26dc6c5d9dcaf698ddac58d44dc1c75242bb6
Alex Adler <project.alex.adler@gmail.com>
2022-01-03 20:37:28 +0100

    Added draft of the introduction to Arean Freight

---
+++ arean_freight/arean_freight.js
@@ -0,0 +1,11 @@
+// Generate a random number for the year
+function yearGlitch() {
+    var year = document.getElementById("year-glitch");
+    var new_year = "2";
+    for (var i = 0; i < 3; i++) {
+        new_year += Math.floor(Math.random() * 10);
+    }
+    year.innerHTML = new_year;
+    setTimeout(yearGlitch, 1000);
+}
+yearGlitch();
