58fa39f895875e5837fb4a7a92a33c378a30e24c
Alex Adler <project.alex.adler@gmail.com>
2022-01-04 08:50:23 +0100

    Using my time system for Aerian Freight now and fixed mobile formatting

--- arean_freight/arean_freight.js
+++ arean_freight/arean_freight.js
@@ -1,11 +1,17 @@
 // Generate a random number for the year
 function yearGlitch() {
     var year = document.getElementById("year-glitch");
-    var new_year = "2";
+    var new_year = "";
+    // 3 digit number
     for (var i = 0; i < 3; i++) {
-        new_year += Math.floor(Math.random() * 10);
+        var num = Math.floor(Math.random() * 10);
+        // If the first digit is 0 then skip it
+        if (!(i === 0 && num === 0)) {
+            new_year += num;
+        }
     }
     year.innerHTML = new_year;
+    // Change the number every second
     setTimeout(yearGlitch, 1000);
 }
 yearGlitch();
